1c66443382de7d5c50e3e40a625964dc8b0e0156
sazan10 <070bex432@ioe.edu.np>
2020-03-30 17:51:29 +0545

    added license info in all files

--- sendData.py
+++ sendData.py
@@ -1,3 +1,25 @@
+/*
+
+   This program is free software: you can redistribute it and/or modify
+
+   it under the terms of the GNU General Public License as published by
+
+   the Free Software Foundation, either version 3 of the License, or
+
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+   GNU General Public License for more details.
+
+   See LICENSE file in the project root for full license information./>.
+
+ */
+ 
 from __future__ import print_function
 from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
 import arm_takeoff as arm
